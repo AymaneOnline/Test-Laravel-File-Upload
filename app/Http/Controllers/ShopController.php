ca677ac44288f0f7ed0d09eb08cb00e2a0cf1862
AymaneOnline <138299321+AymaneOnline@users.noreply.github.com>
2024-09-23 02:35:35 +0100

    My solution

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+
 
 class ShopController extends Controller
 {
     public function store(Request $request)
     {
         $filename = $request->file('photo')->getClientOriginalName();
-        $request->file('photo')->storeAs('shops', $filename);
+        $path = $request->file('photo')->storeAs('shops', $filename);
+
+        // resizing the image
+        $image = Image::make(Storage::path($path))->resize(500, 500);
+
+        // save the resized image as "resized-$filename"
+        $resizedFilename = 'resized-' . $filename;
+        $image->save(Storage::path('shops/' . $resizedFilename));
 
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
